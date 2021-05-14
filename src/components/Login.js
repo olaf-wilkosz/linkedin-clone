25afaa34b8634bd9c24c0b5fa44a4706576359ec
Olaf <67223543+olaf-wilkosz@users.noreply.github.com>
2021-05-14 23:32:10 +0200

    Add and style image and text on login page main area

--- src/components/Login.js
+++ src/components/Login.js
@@ -12,6 +12,12 @@ const Login = (props) => {
           <SignIn>Sign in</SignIn>
         </div>
       </Nav>
+      <Section>
+        <Hero>
+          <h1>Welcome to your professional community</h1>
+          <img src="/images/login-hero.svg" alt="Hero" />
+        </Hero>
+      </Section>
     </Container>
   );
 };
@@ -71,6 +77,62 @@ const SignIn = styled.a`
     background-color: rgba(112, 181, 249, 0.15);
     color: #0a66c2;
     text-decoration: none;
+  }
+`;
+
+const Section = styled.section`
+  display: flex;
+  align-content: start;
+  min-height: 700px;
+  width: 100%;
+  max-width: 1128px;
+  margin: auto;
+  padding-top: 40px;
+  padding-bottom: 138px;
+  padding: 60px 0;
+  position: relative;
+  flex-wrap: wrap;
+  align-items: center;
+
+  @media (max-width: 768px) {
+    margin: auto;
+    min-height: 0;
+  }
+`;
+
+const Hero = styled.div`
+  width: 100%;
+
+  h1 {
+    padding-bottom: 0;
+    width: 55%;
+    font-size: 56px;
+    color: #2977c9;
+    font-weight: 200;
+    line-height: 70px;
+
+    @media (max-width: 768px) {
+      text-align: center;
+      font-size: 20px;
+      width: 100%;
+      line-height: 2;
+    }
+  }
+
+  img {
+    /* z-index: -1; */
+    width: 700px;
+    height: 670px;
+    position: absolute;
+    bottom: -2px;
+    right: -150px;
+
+    @media (max-width: 768px) {
+      top: 230px;
+      width: initial;
+      height: initial;
+      position: initial;
+    }
   }
 `;
 
