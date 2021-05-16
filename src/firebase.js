614a260fbf2a34e12ea2e10a305cfa0203de1006
Olaf <67223543+olaf-wilkosz@users.noreply.github.com>
2021-05-16 23:16:44 +0200

    Set up and connect Firebase

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAfJ1osoN6oHpZyO1-_TJ1TgPwbbcRhR3g",
+  authDomain: "linkedin-clone-47ef7.firebaseapp.com",
+  projectId: "linkedin-clone-47ef7",
+  storageBucket: "linkedin-clone-47ef7.appspot.com",
+  messagingSenderId: "639729609976",
+  appId: "1:639729609976:web:9b6edb397bb818b4b6df0d",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestone();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+const storage = firebaseApp.storage();
+
+export { auth, provider, storage };
+export default db;
