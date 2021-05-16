a5a6620876fd073beb50eed41797d15eda5bb8c7
Olaf <67223543+olaf-wilkosz@users.noreply.github.com>
2021-05-16 23:53:41 +0200

    Add login functionality

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,6 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+import { connect } from "react-redux";
+import { signInAPI } from "../actions";
 
 const Login = (props) => {
   return (
@@ -18,7 +20,7 @@ const Login = (props) => {
           <img src="/images/login-hero.svg" alt="Hero" />
         </Hero>
         <Form>
-          <Google>
+          <Google onClick={() => props.signIn()}>
             <img src="/images/google.svg" alt="Google" />
             Sign in with Google
           </Google>
@@ -161,7 +163,8 @@ const Google = styled.button`
   height: 56px;
   width: 100%;
   border-radius: 28px;
-  box-shadow: inset 0 0 0 1px rgb(0 0 0 / 60%), inset 0 0 0 2px rgb(0 0 0 / 0%) inset 0 0 0 1px rgb(0 0 0 / 0%);
+  box-shadow: inset 0 0 0 1px rgb(0 0 0 / 60%),
+    inset 0 0 0 2px rgb(0 0 0 / 0%) inset 0 0 0 1px rgb(0 0 0 / 0%);
   vertical-align: middle;
   z-index: 0;
   transition-duration: 167ms;
@@ -174,4 +177,12 @@ const Google = styled.button`
   }
 `;
 
-export default Login;
+const mapStateToProps = (state) => {
+  return {};
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  signIn: () => dispatch(signInAPI()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
--- src/firebase.js
+++ src/firebase.js
@@ -10,10 +10,10 @@ const firebaseConfig = {
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
-const db = firebaseApp.firestone();
+const db = firebaseApp.firestore();
 const auth = firebase.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
-const storage = firebaseApp.storage();
+const storage = firebase.storage();
 
 export { auth, provider, storage };
 export default db;
