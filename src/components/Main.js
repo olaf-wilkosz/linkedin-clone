3e58fd0fb4f0986a244af537d364a4c85fa9f7cf
Olaf <67223543+olaf-wilkosz@users.noreply.github.com>
2021-05-16 13:50:36 +0200

    Add & style generic content for main area

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,4 +8,15 @@ const Container = styled.div`
   grid-area: main;
 `;
 
+const CommonCard = styled.div`
+  text-align: center;
+  overflow: hidden;
+  margin-bottom: 8px;
+  background-color: #fff;
+  border-radius: 5px;
+  position: relative;
+  border: none;
+  box-shadow: 0 0 0 1px rgb(0 0 0 / 15%), 0 0 0 rgb(0 0 0 / 20%);
+`;
+
 export default Main;
